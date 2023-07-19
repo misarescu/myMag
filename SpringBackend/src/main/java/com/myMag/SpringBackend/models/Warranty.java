0fafbb72b24e0ba36c7af5ee61274ba13e6ae778
redshadowfx <mihai.isarescu@protonmail.com>
2023-07-19 21:23:01 +0300

    updated the warranty entity

--- SpringBackend/src/main/java/com/myMag/SpringBackend/models/Warranty.java
+++ SpringBackend/src/main/java/com/myMag/SpringBackend/models/Warranty.java
@@ -3,6 +3,7 @@ package com.myMag.SpringBackend.models;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -15,5 +16,57 @@ public class Warranty {
     @OneToOne
     private Product product;
 
+    public Warranty() {
+    }
 
+    public Warranty(LocalDate buyDate, LocalDate expiryDate, Product product) {
+        this.buyDate = buyDate;
+        this.expiryDate = expiryDate;
+        this.product = product;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDate getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(LocalDate buyDate) {
+        this.buyDate = buyDate;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Warranty warranty = (Warranty) o;
+        return Objects.equals(id, warranty.id) && Objects.equals(buyDate, warranty.buyDate) && Objects.equals(expiryDate, warranty.expiryDate) && Objects.equals(product, warranty.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, buyDate, expiryDate, product);
+    }
 }
